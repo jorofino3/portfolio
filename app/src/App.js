31f7a66e7c32b5957fdcb0b2a6fd9bf4f5397437
joeyorofino <97992795+jorofino3@users.noreply.github.com>
2022-08-08 22:10:14 -0400

    bootstrap imports and Navbar changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import "./NavBar.css";
 import { NavBar } from "./componenets/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
+      {/* <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer /> */}
     </div>
   );
 }

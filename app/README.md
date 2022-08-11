18f7444a9f1f61f29b0781e3bb650a0143c674ab
joeyorofino <97992795+jorofino3@users.noreply.github.com>
2022-08-11 15:35:41 -0400

    changes to contact form and .env

--- app/README.md
+++ app/README.md
@@ -68,3 +68,53 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+service: "outlook",
+auth: {
+user: "jorofinospam@gmail.edu",
+pass: "",
+},
+});
+
+contactEmail.verify((error) => {
+if (error) {
+console.log(error);
+} else {
+console.log("Ready to Send");
+}
+});
+
+router.post("/contact", (req, res) => {
+const name = req.body.firstName + req.body.lastName;
+const email = req.body.email;
+const message = req.body.message;
+const phone = req.body.phone;
+const mail = {
+from: name,
+to: "jorofino3@outlook.com",
+subject: "Contact Form Submission - Portfolio",
+html: `<p>Name: ${name}</p> <p>Email: ${email}</p> <p>Phone: ${phone}</p> <p>Message: ${message}</p>`,
+};
+contactEmail.sendMail(mail, (error) => {
+if (error) {
+res.json(error);
+} else {
+res.json({ code: 200, status: "Message Sent" });
+}
+});
+});
--- app/server.js
+++ app/server.js
@@ -1,52 +1,27 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-// server used to send send emails
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
-
-const contactEmail = nodemailer.createTransport({
-  service: "outlook",
+let transporter = nodemailer.createTransport({
+  service: "gmail",
   auth: {
-    user: "jorofinospam@gmail.edu",
-    pass: "Baysboy24joey@",
+    //stop trying to find out my email and password
+    user: "process.env.EMAIL",
+    pass: "process.env.PASSWORD",
   },
 });
 
-contactEmail.verify((error) => {
+let mailOptions = {
+  from: "jorofino12@gmail.com",
+  to: "josephorofino23@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
   if (error) {
+    //handle error
     console.log(error);
   } else {
-    console.log("Ready to Send");
+    console.log("Email sent: " + info.response);
   }
-});
-
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: "jorofino3@outlook.com",
-    subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
-  };
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200, status: "Message Sent" });
-    }
-  });
 });
